aacc7f7551d0551150f00e108bb5f8bc6dfa13ad
santiago <santiagodmaldon@gmail.com>
2023-12-07 06:56:02 -0600

    DeleteMovie Function

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,28 +8,47 @@ export const MainContent = ({moviesList,setMoviesList}) => {
     const getMovies = () => {
         const movies = JSON.parse(localStorage.getItem('movies'));
         setMoviesList(movies);
+
+        return movies
     }
 
-  
+    const deleteMovie = (id) =>{
+       const movies = getMovies();
+       const newMoviesList = movies.filter(movie => movie.id !== id);
 
-    return (
+       setMoviesList(newMoviesList);
+        localStorage.setItem('movies',JSON.stringify(newMoviesList));
+    }
 
-        <>
-            {/* <!-- Main Content --> */}
-            <div className="content">
-           {    
-                    moviesList.map(movie => {
-                        return(
-                            <div className="movie-item" key={movie.id}>
-                                <h3 className="movie-title"> {movie.title}</h3>
-                                <p className="movie-description">
-                                    {movie.description}
-                                </p>
-                                <button className="edit">Edit</button> <button className="delete">Delete</button>
-                            </div>
-                        )
-                })}
-
-            </div></>
-    )
+    if(moviesList){
+        return (
+            <>
+                {/* <!-- Main Content --> */}
+                <div className="content">
+               
+               {     
+                        moviesList.map(movie => {
+                            return(
+                                <div className="movie-item" key={movie.id}>
+                                    <h3 className="movie-title"> {movie.title}</h3>
+                                    <p className="movie-description">
+                                        {movie.description}
+                                    </p>
+                                    <button className="edit">Edit</button> <button className="delete" onClick={ () => deleteMovie(movie.id)} >Delete</button>
+                                </div>
+                            )
+                    })}
+    
+                </div>
+            </>
+        )
+    }
+    else{
+        return(
+            <div className="message">
+                <h3>Theres no movies to show</h3>
+            </div>
+        )
+    }
+   
 }
